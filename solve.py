234684a90d2b9eb86987108d158dc2e7a8850ccc
Lei Fu <leifu@dyn3211-152.wlan.ic.ac.uk>
2020-02-20 17:27:43 +0000

    read line

--- solve.py
+++ solve.py
@@ -1,26 +1,9 @@
 import numpy as np
-import json
+import args
 
-from .score import Car, Score, Ride, check_ride_ids, check_vehicles, eval_ride
+    
 
-def read_input(self):
-        with open(self.input_str, 'r') as f:
-            first_line = f.readline()
-
-            self.rows, self.columns, self.vehicles, self.rides, self.bonus, self.steps = tuple(
-                map(int, first_line.split(' '))
-            )
-
-            self.rides_list = []
-            for i in range(self.rides):
-                self.rides_list.append(tuple(
-                    map(int, f.readline().rstrip().split(' '))
-                ))
-            self.scheduling = [[] for _ in range(self.vehicles)]
-
-        print("Problem statement:")
-        print("Map: {}".format((self.rows, self.columns)),
-              "Vehicles: {}".format(self.vehicles),
-              "Rides: {}".format(self.rides),
-              "Bonus: {}".format(self.bonus),
-              "Steps: {}".format(self.steps))
+if __name__ == "__main__":
+    with open(arg[0], 'r') as f:
+        first_line = f.readline()
+        print(first_line)

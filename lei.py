77467012ca14616c0e1c53e0136420a9c99d01c3
Lei Fu <leifu@dyn3211-152.wlan.ic.ac.uk>
2020-02-20 17:38:19 +0000

    read line

--- lei.py
+++ lei.py
@@ -4,6 +4,8 @@ import sys
     
 
 if __name__ == "__main__":
-    with open(sys.argv[0], 'r') as f:
-        first_line = f.readline()
-        print(first_line)
+    with open(sys.argv[1]) as f:
+        content = f.readlines()
+        content = [x.strip() for x in content] 
+        for s in content:
+            print(s)

e9ceef8f4dd459be0a5ca17456cd456c68dfa0e3
Lei Fu <leifu@dyn3211-152.wlan.ic.ac.uk>
2020-02-20 19:39:37 +0000

    Improved brute force

--- brute.py
+++ brute.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 from Input_reader import Input_reader
 import itertools
+import time
 
 
 
@@ -9,7 +10,13 @@ if __name__ == "__main__":
     input = Input_reader(sys.argv[1])
     sorted_books = sorted(input.books, key=lambda book: book.score, reverse=True)
     max_score = 0
+    for lib in input.libraries:
+        lib.books = sorted(lib.books, key=lambda book: input.dict_books[book].score, reverse=True)
+    iter = 0
     for p in itertools.permutations(input.libraries):
+        iter += 1
+        if (iter > 100):
+            break
         day = 0
         isVisited = np.zeros(shape=(input.num_books))
         output_libs = []
@@ -19,9 +26,8 @@ if __name__ == "__main__":
             available_day = input.num_days - finish_day
             num_books = available_day * l.limits
             count = 0
-            books = sorted(l.books, key=lambda book: input.dict_books[book].score, reverse=True)
             l.output = []
-            for b in books:
+            for b in l.books:
                 if isVisited[b] == 0:
                     isVisited[b] = 1
                     count += 1
@@ -33,16 +39,7 @@ if __name__ == "__main__":
             output_libs.append(l)
             if day > input.num_days:
                 break
-        output_libs = [lib for lib in output_libs if len(lib.output) > 0]
-        f = open("a.txt", "w")
-        f.write(str(len(output_libs)) + "\n")
-        for l in output_libs:
-            print(str(l.id) + " " + str(len(l.output)) + "\n")
-            f.write(str(l.id) + " " + str(len(l.output)) + "\n")
-            for i in l.output:
-                f.write(str(i) + " ")
-            f.write("\n")
-        f.close()
+        
     # print(len(output_libs))
     # for l in output_libs:
     #     if len(l.output) == 0:
@@ -51,3 +48,13 @@ if __name__ == "__main__":
     #     for i in l.output:
     #         print(i, end=" ")
     #     print()
+    output_libs = [lib for lib in output_libs if len(lib.output) > 0]
+    f = open("a.txt", "w")
+    f.write(str(len(output_libs)) + "\n")
+    for l in output_libs:
+        print(str(l.id) + " " + str(len(l.output)) + "\n")
+        f.write(str(l.id) + " " + str(len(l.output)) + "\n")
+        for i in l.output:
+            f.write(str(i) + " ")
+        f.write("\n")
+    f.close()

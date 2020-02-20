8932db2a4e1649605651e62d366a483354247a70
halid1020 <haliteabudureyimu@gmail.com>
2020-02-20 21:23:40 +0000

    lib

--- Library.py
+++ Library.py
@@ -5,6 +5,8 @@ class Library:
         self.duration = duration
         self.limits = limits
         self.output = []
+        self.score = 0
+
     def __str__(self):
         s = ""
         for i in self.books:
